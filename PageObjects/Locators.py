93ae73014e40548efc8eee6b0a01b2ce3512874f
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-09-06 19:41:50 +0530

    Update Locators.py

--- PageObjects/Locators.py
+++ PageObjects/Locators.py
@@ -1,10 +1,10 @@
-# Elements from Login page.
+# Elements from Login page
 field_username = "id:user-name"
 field_password = "id:password"
 button_login = "xpath://*[@id='login-button']"
 
 
-# Elements from Product page.
+# Elements from Product page
 image_after_login = "xpath://*[@id='item_4_img_link']/img"
 hamburger = "id:react-burger-menu-btn"
 logout_link = "xpath://*[@id='logout_sidebar_link']"
